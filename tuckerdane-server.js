27912d88829e9e0e50461b111efe06557e3a6754
Tucker Dane Walker <tuckerdwalker@gmail.com>
2019-09-28 12:35:41 -0700

    Update tuckerdane-server.js

--- tuckerdane-server.js
+++ tuckerdane-server.js
@@ -6,7 +6,7 @@ const port = 8080;
 const server = http.createServer((req, res) => {
 	          res.statusCode = 200;
 	          res.setHeader('Content-Type', 'text/plain');
-	          res.end('continuous integration is working!\n');
+	          res.end('i like turtles\n');
 });
 
 server.listen(port, hostname, () => {
