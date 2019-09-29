710a2e0706899d11af318d2a5b1d99a4c1b234f4
Tucker <tuckerdwalker@gmail.com>
2019-09-29 16:32:19 -0700

    first commit

---
+++ tuckerdane-server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = 'localhost';
+const port = process.argv[2];
+
+const server = http.createServer((req, res) => {
+		          res.statusCode = 200;
+		          res.setHeader('Content-Type', 'text/plain');
+		          res.end('tuckerdane\n');
+});
+
+server.listen(port, hostname, () => {
+		          console.log(`Server running at http://${hostname}:${port}/`);
+});
