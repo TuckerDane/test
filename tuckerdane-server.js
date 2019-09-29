f9f6c31bd7dcc7559f7bb394c27c763d48fc172a
Tucker <tuckerdwalker@gmail.com>
2019-09-29 12:27:20 -0700

    adds process as argument

--- tuckerdane-server.js
+++ tuckerdane-server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = 'localhost';
-const port = 8080;
+const port = process.argv[2];
 
 const server = http.createServer((req, res) => {
 	          res.statusCode = 200;
